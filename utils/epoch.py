5bc4eb54dabdcbd903b2353d90bf6be0a1fc515e
J-Rios <jrios.github@gmail.com>
2020-04-21 21:51:07 +0200

    Updated utils scripts.

--- utils/epoch.py
+++ utils/epoch.py
@@ -1,6 +1,12 @@
-import datetime
+from sys import argv
+from datetime import datetime as dt_datetime
 
-t0 = datetime.datetime(1970, 1, 1, 0, 0, 0, 0)
-t1 = datetime.datetime.now()
+year=1970
+if len(argv) >= 2:
+    year = int(argv[1])
 
+t0 = dt_datetime(year, 1, 1, 0, 0, 0, 0)
+t1 = dt_datetime.now()
+
+print("Seconds since 01/01/{}".format(year))
 print((t1-t0).total_seconds())
--- utils/epoch2000.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-t0 = datetime.datetime(2000, 1, 1, 0, 0, 0, 0)
-t1 = datetime.datetime.now()
-
-print((t1-t0).total_seconds())
